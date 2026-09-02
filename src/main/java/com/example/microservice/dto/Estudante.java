db9ceb477b61f1b048f79b3a0a423afaf5230cd8
docarmojoao4 <docarmojoao4@gmail.com>
2026-09-01 21:40:15 -0300

    adiciona o endpoint para cálculo de média

--- src/main/java/com/example/microservice/dto/Estudante.java
+++ src/main/java/com/example/microservice/dto/Estudante.java
@@ -6,7 +6,7 @@ public record Estudante(
         @NotBlank(message = "O nome do estudante é obrigatório!")
         String nome,
         @DecimalMin(value = "0.0", message = "A nota não pode ser menor que 0!")
-        @DecimalMax(value = "10.0", message = "A nota não podes ser maior que 10!")
+        @DecimalMax(value = "10.0", message = "A nota não pode ser maior que 10!")
         double nota1,
         @DecimalMin(value = "0.0", message = "A nota não pode ser menor que 0!")
         @DecimalMax(value = "10.0", message = "A nota não pode ser maior que 10!")
